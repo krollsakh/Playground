5dc008f855685697bc9d5072cd17f139d4100bb6
krollsakh <kroll@mail.ru>
2024-01-24 23:52:58 +0700

    done

--- src/components/ChartBar.vue
+++ src/components/ChartBar.vue
@@ -4,32 +4,102 @@ import { BarChart } from 'echarts/charts'
 import {
   TooltipComponent,
   LegendComponent,
+  DatasetComponent,
   GridComponent
 } from 'echarts/components'
 import { CanvasRenderer } from 'echarts/renderers'
-
-import VChart from 'vue-echarts'
-import { ChartsOption } from 'components/models'
+import type { DatasetComponentOption } from 'echarts/components'
 
 use([
   TooltipComponent,
   LegendComponent,
+  DatasetComponent,
   GridComponent,
   BarChart,
   CanvasRenderer
 ])
 
-const props = defineProps<{
-  option: ChartsOption
-}>()
+import VChart from 'vue-echarts'
+import { computed } from 'vue'
+import { ECBasicOption } from 'echarts/types/dist/shared'
 
-console.log(props)
+export interface Props {
+  dataset?: DatasetComponentOption
+  loading?: boolean
+}
 
+const props = withDefaults(defineProps<Props>(), {
+  dataset: undefined,
+  loading: false
+})
+
+const option = computed<ECBasicOption>(() => {
+  return {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {},
+    grid: {
+      left: '1%',
+      right: '1%',
+      bottom: '1%',
+      containLabel: true
+    },
+    dataset: props.dataset,
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: props.dataset?.dimensions?.slice(1).map(() => {
+      return {
+        type: 'bar',
+        stack: 'total',
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        barWidth: '90%'
+      }
+    })
+  }
+})
+
+const loadingOptions = {
+  text: 'Загрузка',
+  // color: '#c23531',
+  textColor: '#000',
+  maskColor: 'rgba(255, 255, 255, 0.8)',
+  zlevel: 0,
+
+  // Font size. Available since `v4.8.0`.
+  fontSize: 14,
+  // Show an animated "spinner" or not. Available since `v4.8.0`.
+  showSpinner: true,
+  // Radius of the "spinner". Available since `v4.8.0`.
+  spinnerRadius: 10,
+  // Line width of the "spinner". Available since `v4.8.0`.
+  lineWidth: 3,
+  // Font thick weight. Available since `v5.0.1`.
+  fontWeight: 'normal',
+  // Font style. Available since `v5.0.1`.
+  fontStyle: 'normal',
+  // Font family. Available since `v5.0.1`.
+  fontFamily: 'sans-serif'
+}
 </script>
 
 <template>
   <VChart
     :option="option"
+    :loading="loading"
+    :loading-options="loadingOptions"
     autoresize
   />
 </template>
--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,99 +1,69 @@
 <script setup lang="ts">
+import { DatasetComponentOption } from 'echarts/types/dist/echarts'
 import ChartBar from 'src/components/ChartBar.vue'
-import { ChartsOption } from 'src/components/models'
 import { ref } from 'vue'
 
-const option = ref<ChartsOption>({
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      // Use axis to trigger tooltip
-      type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+const dataset = ref<DatasetComponentOption>()
+const isLoading = ref(false)
+
+const loadData = () => {
+  isLoading.value = true
+  setTimeout(() => {
+    const datasetSrc = {
+      dimensions: ['month', 'Нач.остаток', 'Новые', 'Закрытые', 'Кон.остаток'],
+      source: [
+        { month: 'yan 2023', 'Нач.остаток': 13.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'фев 2023', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'март 2023', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'апр 2023', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 }
+      ]
     }
-  },
-  legend: {},
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: {
-    type: 'value'
-  },
-  yAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  series: [
-    {
-      name: 'Direct',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [320, 302, 301, 334, 390, 330, 320]
-    },
-    {
-      name: 'Mail Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      name: 'Affiliate Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [220, 182, 191, 234, 290, 330, 310]
-    },
-    {
-      name: 'Video Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [150, 212, 201, 154, 190, 330, 410]
-    },
-    {
-      name: 'Search Engine',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [820, 832, 901, 934, 1290, 1330, 1320]
+    dataset.value = datasetSrc
+    isLoading.value = false
+  }, 1000)
+}
+const loadData2 = () => {
+  isLoading.value = true
+  setTimeout(() => {
+    const datasetSrc = {
+      dimensions: ['month', 'Нач.остаток', 'Новые', 'Закрытые', 'Кон.остаток'],
+      source: [
+        { month: 'yan 2023', 'Нач.остаток': 130.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'фев 2023', 'Нач.остаток': 423.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'март 2023', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'апр 2023', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'yan 2024', 'Нач.остаток': 130.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'фев 2025', 'Нач.остаток': 423.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'март 2026', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 },
+        { month: 'апр 2027', 'Нач.остаток': 43.3, Новые: 85.8, Закрытые: 93.7, 'Кон.остаток': 112.5 }
+      ]
     }
-  ]
-})
+    isLoading.value = false
+    dataset.value = datasetSrc
+  }, 1000)
+}
 </script>
 
 <template>
   <div style="height: fit-content;">
     <h3>1</h3>
-    <ChartBar :option="option" class="chart" />
+    <ChartBar
+      :dataset="dataset"
+      :loading="isLoading"
+      class="chart"
+     />
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Go"
+      @click="loadData"
+    />
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Go 2"
+      @click="loadData2"
+    />
     <h3>2</h3>
   </div>
 </template>
